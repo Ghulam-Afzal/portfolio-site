c79e6082a05c0c7791eb4423e76e166f3b0e6961
Ghulam <inferno2960@gmail.com>
2021-04-07 23:33:30 -0400

    added a footer component

---
+++ src/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const Footer = () => { 
+    return ( 
+        <div>
+            <p className='project-desc'>HJDJKAJ</p>
+        </div>
+    )
+}
+
+export default Footer; 
