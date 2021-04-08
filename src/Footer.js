a79e2808d80da302b2f9e602ec9da5ab5bfba60e
Ghulam <inferno2960@gmail.com>
2021-04-08 19:14:28 -0400

    added a footer component

--- src/Footer.js
+++ src/Footer.js
@@ -1,10 +1,17 @@
 import React from 'react';
-
+import './Footer.css';
 
 const Footer = () => { 
+    const LinkedinUrl = 'https://www.pluralsight.com/guides/how-to-render-%22a%22-with-optional-href-in-react'
+    const GithubUrl = 'https://github.com/Ghulam-Afzal'
     return ( 
-        <div>
-            <p className='project-desc'>HJDJKAJ</p>
+        <div className='footer-container'>
+            <div className='socials'>
+                <li><a href ={LinkedinUrl}><button>Home</button></a></li>
+                <li><a href ={LinkedinUrl}><button>Linkedin</button></a></li>
+                <li><a href ={GithubUrl}><button>Github</button></a></li>
+                <li><a href ={GithubUrl}><button>Twitter</button></a></li>
+            </div>
         </div>
     )
 }
