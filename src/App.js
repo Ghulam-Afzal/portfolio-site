9678371eaf54b7295764cd5d3bbfa079b4f223fe
Ghulam <inferno2960@gmail.com>
2021-04-18 14:41:15 -0400

    updated website

--- src/App.js
+++ src/App.js
@@ -3,18 +3,11 @@ import Navbar from './Navbar';
 import HeroContent from './Hero'; 
 import Projects from './Projects';
 import Footer from './Footer';
-import projectData from './projects.json'
 import './App.css'; 
 
 
 
 function App() {
-
-  // let projects = [] 
-
-  // for (let i = 0; i < 5; i++) { 
-  //   projects.push(<Projects />)
-  // }
 
   return (
     <div className="App">
@@ -23,8 +16,7 @@ function App() {
       <div className='section-title'>
         <h1 className='section-name'>Projects</h1>
       </div>
-      {console.log(projectData[0].img)}
-      <Projects title={projectData[0].title} desc={projectData[0].desc} code={projectData[0].code} img={projectData[0].img} link={projectData[0].link}/>
+      <Projects />
       <Footer /> 
     </div>
   );
--- src/Navbar.js
+++ src/Navbar.js
@@ -13,7 +13,6 @@ const Navbar = () => {
                     <li><a href ={sectionID}>Projects</a></li>
                     <li><a href ={LinkedinUrl}>Linkedin</a></li>
                     <li><a href ={GithubUrl}>Github</a></li>
-                    {/* <li><a href ={url}>About</a></li> */}
                 </nav>
             </header>
             
