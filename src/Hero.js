2be52487341797a4f4ef5ca133e4eb78959d8c27
Ghulam <inferno2960@gmail.com>
2021-11-27 15:20:01 -0500

    updated the hero content

--- src/Hero.js
+++ src/Hero.js
@@ -5,7 +5,7 @@ const HeroContent = () => {
     return ( 
         <div id='home' className='hero'>  
             <h1 className='hero-name'>Ghulam Afzal</h1>
-            <p className='hero-about-me'>I am currently a sophomore attending the University of Connecticut as a computer science student. I have a passion for web development, with an increasing interest in game development. When I can take a break from my college work and don't have an idea for a project I like to watch anime or play games. </p>
+            <p className='hero-about-me'>I am currently a junior attending the University of Connecticut as a computer science student. I have a passion for web development, game development, and embedded programming.</p>
         </div>
     )    
 
