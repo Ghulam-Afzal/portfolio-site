97e6174cfd4ec7819852cb1a4ddf971798b391c1
Ghulam <inferno2960@gmail.com>
2021-11-28 17:02:30 -0500

    removed unused link

--- src/Footer.js
+++ src/Footer.js
@@ -1,19 +1,17 @@
 import React from 'react';
 import './Footer.css';
-import { FaGithub, FaTwitter, FaLinkedin, FaHome , FaRegCopyright} from "react-icons/fa";
+import { FaGithub, FaLinkedin, FaHome , FaRegCopyright} from "react-icons/fa";
 
 const Footer = () => { 
     const LinkedinUrl = 'https://www.linkedin.com/'
     const GithubUrl = 'https://github.com/Ghulam-Afzal'
     const homeSectionId = '#home'
-    const twitterLink = 'https://twitter.com/?lang=en'
     return ( 
         <div className='footer-container'>
             <div className='socials'>
                 <li><a href ={homeSectionId}><button><FaHome /></button></a></li>
                 <li><a href ={LinkedinUrl}><button><FaLinkedin /></button></a></li>
                 <li><a href ={GithubUrl}><button><FaGithub /></button></a></li>
-                <li><a href ={twitterLink}><button><FaTwitter /></button></a></li>
             </div>
             <div className='copyright'>
                 <p><FaRegCopyright /> Ghulam Afzal Copyright 2021</p>
